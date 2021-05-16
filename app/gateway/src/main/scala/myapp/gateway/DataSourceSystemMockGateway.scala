7d8a88adfbdf2855ddb7ee5a071d21545217c4c5
Kazuki Negoro <negoro.kazuki@tis.co.jp>
2021-05-16 21:48:32 +0900

    👷chore: モックの改善

--- app/gateway/src/main/scala/myapp/gateway/DataSourceSystemMockGateway.scala
+++ app/gateway/src/main/scala/myapp/gateway/DataSourceSystemMockGateway.scala
@@ -16,9 +16,35 @@ class DataSourceSystemMockGateway(config: GatewayConfig) {
     wireMock.resetMappings()
     wireMock.register(
       get(urlPathEqualTo("/data"))
-        .atPriority(1)
+        .withQueryParam("cursor", matching("(0|[1-9][0-9]*)"))
+        .withQueryParam("limit", matching("[1-9][0-9]*"))
+        .willReturn(
+          ok()
+            .withHeader("Content-Type", "application/json")
+            .withBody {
+              """
+              {{#assign 'limit'}}
+                {{#if (compare request.query.cursor '<' 10000)}}
+                  {{request.query.limit}}
+                {{else}}
+                  {{sub request.query.limit 1}}
+                {{/if}}
+              {{/assign}}
+              {
+                 "data": [
+                    {{#each (seq from=(add request.query.cursor 1) count=limit) ~}}
+                    { "cursor": "{{this}}", "accountNo": "{{randomValue length=1 type='NUMERIC'}}", "amount": 1000 }{{#unless @last}},{{/unless}}
+                    {{/each}}
+                 ]
+              }
+              """
+            },
+        ),
+    )
+    wireMock.register(
+      get(urlPathEqualTo("/data"))
         .withQueryParam("cursor", absent())
-        .withQueryParam("limit", matching("1000"))
+        .withQueryParam("limit", matching("[1-9][0-9]*"))
         .willReturn(
           ok()
             .withHeader("Content-Type", "application/json")
@@ -26,66 +52,9 @@ class DataSourceSystemMockGateway(config: GatewayConfig) {
               """
               {
                  "data": [
-                    {{#each parameters.numbers ~}}
+                    {{#each (seq from=1 count=request.query.limit) ~}}
                     { "cursor": "{{this}}", "accountNo": "{{randomValue length=1 type='NUMERIC'}}", "amount": 1000 }{{#unless @last}},{{/unless}}
                     {{/each}}
-                 ]
-              }
-              """
-            }.withTransformerParameter("numbers", (1 to 1000).asJava),
-        ),
-    )
-    wireMock.register(
-      get(urlPathEqualTo("/data"))
-        .withQueryParam("cursor", matching("1000"))
-        .withQueryParam("limit", matching("1000"))
-        .willReturn(
-          ok()
-            .withHeader("Content-Type", "application/json")
-            .withBody {
-              """
-              {
-                 "data": [
-                    {{#each parameters.numbers ~}}
-                    { "cursor": "{{this}}", "accountNo": "{{randomValue length=1 type='NUMERIC'}}", "amount": 1000 }{{#unless @last}},{{/unless}}
-                    {{/each}}
-                 ]
-              }
-              """
-            }.withTransformerParameter("numbers", (1001 to 2000).asJava),
-        ),
-    )
-    wireMock.register(
-      get(urlPathEqualTo("/data"))
-        .withQueryParam("cursor", matching("2000"))
-        .withQueryParam("limit", matching("1000"))
-        .willReturn(
-          ok()
-            .withHeader("Content-Type", "application/json")
-            .withBody {
-              """
-              {
-                 "data": [
-                    {{#each parameters.numbers ~}}
-                    { "cursor": "{{this}}", "accountNo": "{{randomValue length=1 type='NUMERIC'}}", "amount": 1000 }{{#unless @last}},{{/unless}}
-                    {{/each}}
-                 ]
-              }
-              """
-            }.withTransformerParameter("numbers", (2001 to 2010).asJava),
-        ),
-    )
-    wireMock.register(
-      get(urlPathEqualTo("/data"))
-        .withQueryParam("cursor", matching("2010"))
-        .withQueryParam("limit", matching("1000"))
-        .willReturn(
-          ok()
-            .withHeader("Content-Type", "application/json")
-            .withBody {
-              """
-              {
-                 "data": [
                  ]
               }
               """
