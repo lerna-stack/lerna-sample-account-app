b518b1b1c27fe07b698cbd53baddaee82c3f7c4e
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-07-01 13:05:31 +0900

    feat: /accounts route を追加

--- app/presentation/src/main/scala/myapp/presentation/application/ApplicationRoute.scala
+++ app/presentation/src/main/scala/myapp/presentation/application/ApplicationRoute.scala
@@ -3,11 +3,47 @@ package myapp.presentation.application
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import myapp.adapter.account.{ AccountNo, BankAccountApplication, TransactionId }
 
-class ApplicationRoute {
+class ApplicationRoute(bankAccountApplication: BankAccountApplication) {
+  import ApplicationRoute._
+
   def route: Route = concat(
     path("index") {
       complete(StatusCodes.OK -> "OK")
     },
+    pathPrefix("accounts" / Segment.map(AccountNo)) { accountNo =>
+      concat(
+        get {
+          onSuccess(bankAccountApplication.fetchBalance(accountNo)) { result =>
+            complete(result.toString + "\n")
+          }
+        },
+        (post & parameters("amount".as[Int], "transactionId".asTransactionId)) { (amount, transactionId) =>
+          concat(
+            path("deposit") {
+              onSuccess(bankAccountApplication.deposit(accountNo, transactionId, amount)) { result =>
+                complete(result.toString + "\n")
+              }
+            },
+            path("withdraw") {
+              onSuccess(bankAccountApplication.withdraw(accountNo, transactionId, amount)) { result =>
+                complete(result.toString + "\n")
+              }
+            },
+          )
+        },
+      )
+    },
   )
+}
+
+object ApplicationRoute {
+  implicit class NameUnmarshallerReceptacleOps(name: String) {
+    import akka.http.scaladsl.common.NameUnmarshallerReceptacle
+    import akka.http.scaladsl.unmarshalling.Unmarshaller
+
+    def asTransactionId: NameUnmarshallerReceptacle[TransactionId] =
+      name.as[Long].as[TransactionId](Unmarshaller.strict(TransactionId.apply))
+  }
 }
--- app/presentation/src/test/scala/myapp/presentation/application/ApplicationRouteSpec.scala
+++ app/presentation/src/test/scala/myapp/presentation/application/ApplicationRouteSpec.scala
@@ -3,13 +3,31 @@ package myapp.presentation.application
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.testkit.ScalatestRouteTest
 import lerna.testkit.airframe.DISessionSupport
+import myapp.adapter.account.{ AccountNo, BankAccountApplication, TransactionId }
 import myapp.presentation.PresentationDIDesign
 import myapp.utility.scalatest.StandardSpec
 import wvlet.airframe.Design
 
+import scala.concurrent.Future
+
 @SuppressWarnings(Array("org.wartremover.contrib.warts.MissingOverride"))
 class ApplicationRouteSpec extends StandardSpec with ScalatestRouteTest with DISessionSupport {
   override protected val diDesign: Design = PresentationDIDesign.presentationDesign
+    .bind[BankAccountApplication].toInstance(new BankAccountApplication {
+      override def fetchBalance(
+          accountNo: AccountNo,
+      ): Future[BigDecimal] = ???
+      override def deposit(
+          accountNo: AccountNo,
+          transactionId: TransactionId,
+          amount: Int,
+      ): Future[BigDecimal] = ???
+      override def withdraw(
+          accountNo: AccountNo,
+          transactionId: TransactionId,
+          amount: Int,
+      ): Future[BigDecimal] = ???
+    })
 
   val route: ApplicationRoute = diSession.build[ApplicationRoute]
 
