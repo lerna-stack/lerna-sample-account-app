7dad5348bce864037dc746d98b9de1daef256d05
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-07-02 12:08:04 +0900

    chore: g8 template を元に各種library・初期コードなどを最新化
    lerna-stack/lerna.g8 at develop
    https://github.com/lerna-stack/lerna.g8/tree/develop

--- app/entrypoint/src/main/scala/myapp/entrypoint/DIDesign.scala
+++ app/entrypoint/src/main/scala/myapp/entrypoint/DIDesign.scala
@@ -1,6 +1,6 @@
 package myapp.entrypoint
 
-import akka.actor.ActorSystem
+import akka.actor.typed.ActorSystem
 import com.typesafe.config.Config
 import myapp.presentation.PresentationDIDesign
 import wvlet.airframe._
@@ -8,10 +8,10 @@ import wvlet.airframe._
 @SuppressWarnings(Array("org.wartremover.contrib.warts.MissingOverride"))
 object DIDesign {
   @SuppressWarnings(Array("lerna.warts.CyclomaticComplexity"))
-  def design(system: ActorSystem): Design =
+  def design(system: ActorSystem[Nothing]): Design =
     newDesign
-      .bind[ActorSystem].toInstance(system)
+      .bind[ActorSystem[Nothing]].toInstance(system)
       .bind[MyApp].toSingleton
-      .bind[Config].toSingletonProvider[ActorSystem] { system => system.settings.config }
+      .bind[Config].toSingletonProvider[ActorSystem[Nothing]] { system => system.settings.config }
       .add(PresentationDIDesign.presentationDesign)
 }
