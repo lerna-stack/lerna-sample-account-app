a51f280efad32da5d0ec222753f04000bbbc689f
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-07-09 11:25:59 +0900

    perf: TransactionId の シリアライズを効率化
    Number のみをシリアライズする

---
+++ app/application/src/main/scala/myapp/application/serialization/adapter/account/TransactionIdJacksonModule.scala
@@ -0,0 +1,32 @@
+package myapp.application.serialization.adapter.account
+
+import com.fasterxml.jackson.core.{ JsonGenerator, JsonParser }
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
+import com.fasterxml.jackson.databind.{ DeserializationContext, SerializerProvider }
+import myapp.adapter.account.TransactionId
+
+class TransactionIdJacksonModule extends SimpleModule {
+  import TransactionIdJacksonModule._
+
+  addSerializer(new JsonSerializer)
+  addDeserializer(classOf[TransactionId], new JsonDeserializer)
+}
+
+object TransactionIdJacksonModule {
+  private class JsonSerializer extends StdSerializer[TransactionId](classOf[TransactionId]) {
+    override def serialize(
+        transactionId: TransactionId,
+        jsonGenerator: JsonGenerator,
+        provider: SerializerProvider,
+    ): Unit = {
+      jsonGenerator.writeNumber(transactionId.value)
+    }
+  }
+
+  private class JsonDeserializer extends StdDeserializer[TransactionId](classOf[TransactionId]) {
+    override def deserialize(p: JsonParser, context: DeserializationContext): TransactionId =
+      TransactionId(p.getNumberValue.longValue())
+  }
+}
