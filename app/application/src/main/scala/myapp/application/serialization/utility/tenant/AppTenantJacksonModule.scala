ea31e146356bdfcd17a66eae395fc3536a95b8cd
杉本 貴史 <sugimoto.takashi@tis.co.jp>
2021-07-09 10:46:44 +0900

    refactor: jacksonModule を SimpleModule 化
    コード量削減

--- app/application/src/main/scala/myapp/application/serialization/utility/tenant/AppTenantJacksonModule.scala
+++ app/application/src/main/scala/myapp/application/serialization/utility/tenant/AppTenantJacksonModule.scala
@@ -1,43 +1,17 @@
 package myapp.application.serialization.utility.tenant
 
 import com.fasterxml.jackson.core.{ JsonGenerator, JsonParser }
-import com.fasterxml.jackson.databind
-import com.fasterxml.jackson.databind.deser.Deserializers
-import com.fasterxml.jackson.databind.deser.std.StdDeserializer
-import com.fasterxml.jackson.databind.ser.Serializers
-import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import com.fasterxml.jackson.databind._
-import com.fasterxml.jackson.module.scala.JacksonModule
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer
+import com.fasterxml.jackson.databind.module.SimpleModule
+import com.fasterxml.jackson.databind.ser.std.StdSerializer
 import myapp.utility.tenant.AppTenant
 
-class AppTenantJacksonModule extends JacksonModule {
+class AppTenantJacksonModule extends SimpleModule {
   import AppTenantJacksonModule._
-  override def getModuleName(): String = this.getClass.getName
 
-  +=(new Serializers.Base {
-    private val serializer = new JsonSerializer
-    override def findSerializer(
-        config: SerializationConfig,
-        javaType: JavaType,
-        beanDesc: BeanDescription,
-    ): databind.JsonSerializer[_] = {
-      if (serializer.handledType().isAssignableFrom(javaType.getRawClass)) serializer
-      else super.findSerializer(config, javaType, beanDesc)
-    }
-  })
-
-  +=(new Deserializers.Base {
-    private val deserializer = new JsonDeserializer
-
-    override def findBeanDeserializer(
-        javaType: JavaType,
-        config: DeserializationConfig,
-        beanDesc: BeanDescription,
-    ): databind.JsonDeserializer[_] = {
-      if (deserializer.handledType().isAssignableFrom(javaType.getRawClass)) deserializer
-      else super.findBeanDeserializer(javaType, config, beanDesc)
-    }
-  })
+  addSerializer(new JsonSerializer)
+  addDeserializer(classOf[AppTenant], new JsonDeserializer)
 }
 
 object AppTenantJacksonModule {
