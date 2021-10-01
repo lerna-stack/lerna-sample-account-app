12af708605d555e8e52fd41a14497eaf1843a51a
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-10-01 16:59:17 +0900

    doc: BankAccountApplication の apidoc を分かりやすく改訂する
    * 入金に使用する TransactionId を 入金ID と表現する
    * 出金に使用する TransactionId を 出金ID と表現する
    * 説明につながりができるように文の順序を変更する

--- app/adapter/src/main/scala/myapp/adapter/account/BankAccountApplication.scala
+++ app/adapter/src/main/scala/myapp/adapter/account/BankAccountApplication.scala
@@ -23,23 +23,23 @@ trait BankAccountApplication {
   /** 指定口座に指定金額を入金する。
     *
     * 口座は、口座番号([[AccountNo]])とテナント([[myapp.utility.tenant.AppTenant]])で一意に決定される。
-    * `transactionId` により冪等性が保証される。
     *
     * 入金に成功した場合、入金後の残高を含む [[DepositResult.Succeeded]] を返す。
-    *
     * 想定される失敗は、[[DepositResult]] として返す。
     * 想定できない失敗は、失敗した [[Future]] として返す。
-    * 失敗の種類によっては、同じ `transactionId` を指定してリトライする方が良い。
+    *
+    * 失敗の種類によっては、同じ 入金ID を指定してリトライする方が良い。
     * 例えば、 [[DepositResult.Timeout]] や 失敗した Future が返された場合は、
     * この入金が完了したかどうか(成功したか、失敗したか)を判断できない。
-    * このような場合には、同じ `transactionId` を指定してリトライする方が良い。
+    * このような場合には、同じ 入金ID を指定してリトライする方が良い。
+    * 入金ID により冪等性が保証されている。
     *
-    * 同じ `transactionId` を使用してリトライする場合、前回とまったく同じ入金金額を指定するべきである。
+    * 同じ 入金ID を使用してリトライする場合、前回とまったく同じ入金金額を指定するべきである。
     * 異なる入金金額を指定して成功が返った場合、実際にいくら入金できたのかをこのメソッドの戻り値からは知ることはできない。
     * 今後の更新によって、このような場合に失敗を返すように変更される可能性がある。
     *
     * @param accountNo 口座番号
-    * @param transactionId トランザクションID
+    * @param transactionId 入金ID
     * @param amount 入金金額
     * @return 入金結果を格納した [[Future]] を返す
     */
@@ -52,23 +52,23 @@ trait BankAccountApplication {
   /** 指定口座から指定金額を出金する。
     *
     * 口座は、口座番号([[AccountNo]])とテナント([[myapp.utility.tenant.AppTenant]])で一意に決定される。
-    * `transactionId` により冪等性が保証される。
     *
     * 出金に成功した場合、出金後の残高を含む [[WithdrawalResult.Succeeded]] を返す。
-    *
     * 想定される失敗は、[[WithdrawalResult]] として返す。
     * 想定できない失敗は、失敗した [[Future]] として返す。
-    * 失敗の種類によっては、同じ `transactionId` を指定してリトライする方が良い。
+    *
+    * 失敗の種類によっては、同じ 出金ID を指定してリトライする方が良い。
     * 例えば、 [[WithdrawalResult.Timeout]] や 失敗した Future が返された場合は、
     * この出金が完了したかどうか(成功したか、失敗したか)を判断できない。
-    * このような場合には、同じ `transactionId` を指定してリトライする方が良い。
+    * このような場合には、同じ 出金ID を指定してリトライする方が良い。
+    * 出金ID により冪等性が保証されている。
     *
-    * 同じ `transactionId` を使用してリトライする場合、前回とまったく同じ出金金額を指定するべきである。
+    * 同じ 出金ID を使用してリトライする場合、前回とまったく同じ出金金額を指定するべきである。
     * 異なる出金金額を指定して成功が返った場合、実際にいくら出金できたのかをこのメソッドの戻り値からは知ることはできない。
     * 今後の更新によって、このような場合に失敗を返すように変更される可能性がある。
     *
     * @param accountNo 口座番号
-    * @param transactionId トランザクションID
+    * @param transactionId 出金ID
     * @param amount 出金金額
     * @return 出金結果を格納した [[Future]] を返す
     */
@@ -110,7 +110,7 @@ object BankAccountApplication {
 
     /** 入金失敗: 残高不足
       *
-      * @note 同じ [[TransactionId]] を使用してリトライ '''できない'''
+      * @note 同じ 入金ID を使用してリトライ '''できない'''
       */
     case object ExcessBalance extends DepositResult
 
@@ -120,7 +120,7 @@ object BankAccountApplication {
       *
       * タイムアウトが発生した場合、入金が処理されたかどうか(成功したか、失敗したか)を知ることができない。
       *
-      * @note 同じ [[TransactionId]] を使用してリトライ '''できる'''
+      * @note 同じ 入金ID を使用してリトライ '''できる'''
       */
     case object Timeout extends DepositResult
 
@@ -137,7 +137,7 @@ object BankAccountApplication {
 
     /** 出金失敗: 残高不足
       *
-      * @note 同じ [[TransactionId]] を使用してリトライ'''できない'''
+      * @note 同じ 出金ID を使用してリトライ'''できない'''
       */
     case object ShortBalance extends WithdrawalResult
 
@@ -147,7 +147,7 @@ object BankAccountApplication {
       *
       * タイムアウトが発生した場合、出金が処理されたかどうか(成功したか、失敗したか)を知ることができない。
       *
-      * @note 同じ [[TransactionId]] を使用してリトライ'''できる'''
+      * @note 同じ 出金ID を使用してリトライ'''できる'''
       */
     case object Timeout extends WithdrawalResult
 
