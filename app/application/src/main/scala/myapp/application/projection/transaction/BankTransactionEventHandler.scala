91ef268f90c2e22470a56169be344fd7acbf0ab9
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-10-04 10:20:09 +0900

    feat: BankAccountBehavior に返金機能を追加する

--- app/application/src/main/scala/myapp/application/projection/transaction/BankTransactionEventHandler.scala
+++ app/application/src/main/scala/myapp/application/projection/transaction/BankTransactionEventHandler.scala
@@ -4,7 +4,14 @@ import akka.Done
 import akka.projection.eventsourced.EventEnvelope
 import lerna.log.AppLogging
 import myapp.application.account.{ BankAccountBehavior, BankAccountEventAdapter }
-import myapp.application.account.BankAccountBehavior.{ BalanceExceeded, BalanceShorted, Deposited, Withdrew }
+import myapp.application.account.BankAccountBehavior.{
+  BalanceExceeded,
+  BalanceShorted,
+  Deposited,
+  InvalidRefundRequested,
+  Refunded,
+  Withdrew,
+}
 import myapp.application.persistence.AggregateEventTag
 import myapp.application.projection.AppEventHandler
 import myapp.utility.AppRequestContext
@@ -30,6 +37,22 @@ class BankTransactionEventHandler extends AppEventHandler[BankAccountBehavior.Do
         DBIO.successful(Done)
       case BalanceShorted(transactionId) =>
         logger.info("BalanceShorted(transactionId: {})", transactionId)
+        DBIO.successful(Done)
+      case Refunded(transactionId, withdrawalTransactionId, amount) =>
+        logger.info(
+          "Refunded(transactionId: {}, withdrawalTransactionId: {}, amount: {})",
+          transactionId,
+          withdrawalTransactionId,
+          amount,
+        )
+        DBIO.successful(Done)
+      case InvalidRefundRequested(transactionId, withdrawalTransactionId, amount) =>
+        logger.info(
+          "InvalidRefundRequested(transactionId: {}, withdrawalTransactionId: {}, amount: {}",
+          transactionId,
+          withdrawalTransactionId,
+          amount,
+        )
         DBIO.successful(Done)
     }
   }
