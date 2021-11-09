6e00ef17c53d7caace7726e6cd9424e6ae404abc
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-11-09 15:22:16 +0900

    refactor: Tablesからdatabaseconfigを取得するように変更

--- app/application/src/main/scala/myapp/application/projection/transaction/TransactionRepository.scala
+++ app/application/src/main/scala/myapp/application/projection/transaction/TransactionRepository.scala
@@ -2,22 +2,19 @@ package myapp.application.projection.transaction
 
 import akka.Done
 import myapp.readmodel.schema.Tables
-import slick.basic.DatabaseConfig
 import slick.dbio.DBIO
-import slick.jdbc.JdbcProfile
 
 import scala.concurrent.ExecutionContext
 
-final case class Transaction(transactionId: String, eventName: String, amount: Int)
+final case class Transaction(transactionId: String, eventName: String, amount: Long)
 
 trait TransactionRepository {
   def save(transaction: Transaction)(implicit ec: ExecutionContext): DBIO[Done]
 }
 
-class TransactionRepositoryImpl(val table: Tables, dbConfig: DatabaseConfig[JdbcProfile])
-    extends TransactionRepository {
-  import dbConfig.profile.api._
-  import table._
+class TransactionRepositoryImpl(tables: Tables) extends TransactionRepository {
+  import tables._
+  import tables.profile.api._
   override def save(transaction: Transaction)(implicit ec: ExecutionContext) = {
     TransactionStore
       .insertOrUpdate(TransactionStoreRow(transaction.transactionId, transaction.eventName, transaction.amount))
