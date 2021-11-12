dec173acc0609888a41c7d21698777ec206160e8
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-11-12 18:26:43 +0900

    test: refactor: DB操作も含めてテストできるように修正

--- app/application/src/test/scala/myapp/application/projection/transaction/BankTransactionEventHandlerSpec.scala
+++ app/application/src/test/scala/myapp/application/projection/transaction/BankTransactionEventHandlerSpec.scala
@@ -1,83 +1,88 @@
 package myapp.application.projection.transaction
 
-import akka.Done
+import akka.actor.typed.ActorSystem
 import akka.persistence.query.Offset
-import akka.projection.ProjectionId
+import akka.projection.Projection
 import akka.projection.eventsourced.EventEnvelope
-import akka.projection.scaladsl.Handler
-import akka.projection.testkit.scaladsl.{ ProjectionTestKit, TestProjection, TestSourceProvider }
+import akka.projection.scaladsl.SourceProvider
+import akka.projection.testkit.scaladsl.{ ProjectionTestKit, TestSourceProvider }
 import akka.stream.scaladsl.Source
+import com.typesafe.config.Config
+import lerna.testkit.airframe.DISessionSupport
 import lerna.testkit.akka.ScalaTestWithTypedActorTestKit
 import lerna.util.trace.TraceId
 import myapp.adapter.account.TransactionId
 import myapp.application.account.BankAccountBehavior.{ Deposited, DomainEvent, Refunded, Withdrew }
-import myapp.application.projection.AppEventHandler
+import myapp.application.projection.AppEventHandler.BehaviorSetup
+import myapp.readmodel.{ JDBCSupport, ReadModeDIDesign }
 import myapp.utility.AppRequestContext
+import myapp.utility.scalatest.StandardSpec
 import myapp.utility.tenant.TenantA
-import org.scalatest.wordspec.AnyWordSpecLike
-import slick.dbio.DBIO
+import wvlet.airframe.{ newDesign, Design }
 
-import scala.concurrent.{ ExecutionContext, ExecutionContextExecutor, Future }
+@SuppressWarnings(
+  Array(
+    "org.wartremover.contrib.warts.MissingOverride",
+    "org.wartremover.warts.Equals",
+    "org.wartremover.warts.IsInstanceOf",
+  ),
+)
+class BankTransactionEventHandlerSpec
+    extends ScalaTestWithTypedActorTestKit()
+    with StandardSpec
+    with DISessionSupport
+    with JDBCSupport {
 
-object BankTransactionEventHandlerSpec {
-  class MockTransactionRepository extends TransactionRepository {
-    @SuppressWarnings(Array("org.wartremover.warts.Var"))
-    var table: Vector[Transaction] = Vector[Transaction]()
-
-    override def save(transaction: Transaction)(implicit ec: ExecutionContext): DBIO[Done] = DBIO.successful {
-      table :+= transaction
-      Done
-    }
-  }
-}
-
-class BankTransactionEventHandlerSpec extends ScalaTestWithTypedActorTestKit() with AnyWordSpecLike {
-  import BankTransactionEventHandlerSpec._
-
-  private val projectionTestKit = ProjectionTestKit(system)
-
-  def createEnvelope(event: DomainEvent, seqNo: Long, timestamp: Long = 0L): EventEnvelope[DomainEvent] =
-    EventEnvelope(Offset.sequence(seqNo), "persistenceId", seqNo, event, timestamp)
-
-  def converter(
-      handler: AppEventHandler[DomainEvent],
-  )(implicit ec: ExecutionContext): Handler[EventEnvelope[DomainEvent]] = { envelope: EventEnvelope[DomainEvent] =>
-    Future {
-      handler.process(envelope)
-      Done
-    }
-  }
+  override protected val diDesign: Design = newDesign
+    .add(ReadModeDIDesign.readModelDDesign)
+    .bind[Config].toInstance(testKit.config)
+    .bind[ActorSystem[Nothing]].toInstance(system)
+    .bind[TransactionRepository].to[TransactionRepositoryImpl]
 
   "BankTransactionEventHandler" should {
-    "process transaction events" in {
-      implicit val ec: ExecutionContextExecutor         = system.executionContext
-      implicit val appRequestContext: AppRequestContext = AppRequestContext(TraceId("trace_id"), TenantA)
-      val repository                                    = new MockTransactionRepository()
-      val handler                                       = new BankTransactionEventHandler(system, repository)
+    import tableSeeds._
+    import tables._
+    import tables.profile.api._
 
-      val events = Source(
-        List(
-          createEnvelope(Deposited(TransactionId("id0"), BigInt(100000)), 0L),
-          createEnvelope(Withdrew(TransactionId("id1"), BigInt(5000)), 1L),
-          createEnvelope(Withdrew(TransactionId("id2"), BigInt(10000)), 2L),
-          createEnvelope(Refunded(TransactionId("id3"), TransactionId("id2"), BigInt(2000)), 3L),
-          createEnvelope(Deposited(TransactionId("id4"), BigInt(200000)), 4L),
-        ),
+    val projectionTestKit = ProjectionTestKit(system)
+    val setup             = BehaviorSetup(system, jdbcService.dbConfig, tenant)
+
+    def createEnvelope(event: DomainEvent, seqNo: Long, timestamp: Long = 0L): EventEnvelope[DomainEvent] =
+      EventEnvelope(Offset.sequence(seqNo), "persistenceId", seqNo, event, timestamp)
+
+    def createProjection(requests: EventEnvelope[DomainEvent]*): Projection[EventEnvelope[DomainEvent]] = {
+      val source = Source(requests)
+      val sourceProvider: SourceProvider[Offset, EventEnvelope[DomainEvent]] =
+        TestSourceProvider[Offset, EventEnvelope[DomainEvent]](source, extractOffset = env => env.offset)
+      diSession.build[BankTransactionEventHandler].createProjection(setup, sourceProvider)
+    }
+
+    "process transaction events" in withJDBC { db =>
+      implicit val appRequestContext: AppRequestContext = AppRequestContext(TraceId("trace_id"), TenantA)
+
+      val events: Vector[DomainEvent] = Vector[DomainEvent](
+        Deposited(TransactionId("id0"), BigInt(100000)),
+        Withdrew(TransactionId("id1"), BigInt(5000)),
+        Withdrew(TransactionId("id2"), BigInt(10000)),
+        Refunded(TransactionId("id3"), TransactionId("id2"), BigInt(2000)),
+        Deposited(TransactionId("id4"), BigInt(200000)),
       )
 
-      val projectionId = ProjectionId("name", "key")
-      val sourceProvider =
-        TestSourceProvider[Offset, EventEnvelope[DomainEvent]](events, extractOffset = env => env.offset)
-      val projection = TestProjection(projectionId, sourceProvider, () => converter(handler))
+      val envelopedEvents: Vector[EventEnvelope[DomainEvent]] = events.zipWithIndex.map {
+        case (event, i) => createEnvelope(event, i)
+      }
 
+      val projection: Projection[EventEnvelope[DomainEvent]] = createProjection(envelopedEvents: _*)
+
+      val expected: Vector[TransactionStoreRow] = events.map {
+        case Deposited(id, amount)   => TransactionStoreRow(id.value, "Deposited", amount.longValue)
+        case Withdrew(id, amount)    => TransactionStoreRow(id.value, "Withdrew", amount.longValue)
+        case Refunded(id, _, amount) => TransactionStoreRow(id.value, "Refunded", amount.longValue)
+      }
       projectionTestKit.run(projection) {
-        repository.table shouldBe Vector(
-          Transaction("id0", "Deposited", 100000),
-          Transaction("id1", "Withdrew", 5000),
-          Transaction("id2", "Withdrew", 10000),
-          Transaction("id3", "Refunded", 2000),
-          Transaction("id4", "Deposited", 200000),
-        )
+        db.validate(TransactionStore.result) { result =>
+          result should contain theSameElementsAs expected
+        }
       }
     }
   }
