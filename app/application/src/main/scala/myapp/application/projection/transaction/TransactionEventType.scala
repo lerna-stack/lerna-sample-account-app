7eb2de99c5bf18e28812f7c878302647284dd777
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-11-16 16:33:52 +0900

    refactor: TransactionEventType.Valueの別名を定義

--- app/application/src/main/scala/myapp/application/projection/transaction/TransactionEventType.scala
+++ app/application/src/main/scala/myapp/application/projection/transaction/TransactionEventType.scala
@@ -1,6 +1,7 @@
 package myapp.application.projection.transaction
 
 object TransactionEventType extends Enumeration {
+  type TransactionEventType = Value
   val Deposited = Value("Deposited")
   val Withdrew  = Value("Withdrew")
   val Refunded  = Value("Refunded")
