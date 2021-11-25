e88614e443237d24bfb6c1019542657af3034009
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-11-25 18:22:18 +0900

    refactor: constructorをprivateに変更

--- app/presentation/src/main/scala/myapp/presentation/application/AccountStatementResponse.scala
+++ app/presentation/src/main/scala/myapp/presentation/application/AccountStatementResponse.scala
@@ -1,22 +1,24 @@
 package myapp.presentation.application
 
-import lerna.http.json.AnyValJsonFormat
 import myapp.adapter.account.{ AccountNo, TransactionDto }
 import myapp.utility.tenant.AppTenant
 import spray.json.{ JsonFormat, RootJsonFormat }
 
-final case class AccountStatementResponse(accountNo: AccountNo, tenant: String, transactions: Seq[TransactionDto])
+final case class AccountStatementResponse private (
+    accountNo: String,
+    tenant: String,
+    transactions: Seq[TransactionDto],
+)
 
 object AccountStatementResponse {
   def from(accountNo: AccountNo, tenant: AppTenant, transactionList: Seq[TransactionDto]): AccountStatementResponse =
-    AccountStatementResponse(accountNo, tenant.id, transactionList)
+    AccountStatementResponse(accountNo.value, tenant.id, transactionList)
 
   import spray.json.DefaultJsonProtocol._
 
-  implicit private val accountNoJsonFormat: JsonFormat[AccountNo]        = AnyValJsonFormat(AccountNo.apply, AccountNo.unapply)
   implicit private val transactionJsonFormat: JsonFormat[TransactionDto] = jsonFormat5(TransactionDto)
   implicit val accountStatementJsonFormat: RootJsonFormat[AccountStatementResponse] =
-    jsonFormat3((accountNo: AccountNo, tenant: String, transactions: Seq[TransactionDto]) =>
+    jsonFormat3((accountNo: String, tenant: String, transactions: Seq[TransactionDto]) =>
       AccountStatementResponse.apply(accountNo, tenant, transactions),
     )
 }
