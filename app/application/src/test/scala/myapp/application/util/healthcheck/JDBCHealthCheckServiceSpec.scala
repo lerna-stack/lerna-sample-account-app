b6429c35be0a048191a73e7453a34712f9d7342c
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-12-10 17:54:00 +0900

    test: JDBCHealthCheckServiceのテストを追加

---
+++ app/application/src/test/scala/myapp/application/util/healthcheck/JDBCHealthCheckServiceSpec.scala
@@ -0,0 +1,74 @@
+package myapp.application.util.healthcheck
+
+import akka.actor.testkit.typed.scaladsl.TestProbe
+import akka.actor.typed.ActorRef
+import com.typesafe.config.{ Config, ConfigFactory }
+import lerna.testkit.airframe.DISessionSupport
+import lerna.testkit.akka.ScalaTestWithTypedActorTestKit
+import myapp.utility.scalatest.StandardSpec
+import org.scalamock.function.MockFunction0
+import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfterAll
+import wvlet.airframe.{ newDesign, Design }
+
+import scala.concurrent.Future
+
+@SuppressWarnings(Array("org.wartremover.contrib.warts.MissingOverride"))
+class JDBCHealthCheckServiceSpec
+    extends ScalaTestWithTypedActorTestKit
+    with StandardSpec
+    with BeforeAndAfterAll
+    with MockFactory
+    with DISessionSupport {
+  import JDBCHealthCheckService._
+
+  @SuppressWarnings(
+    Array(
+      "org.wartremover.warts.Equals",
+      "org.wartremover.warts.Null",
+      "org.wartremover.warts.Throw",
+    ),
+  )
+  override protected val diDesign: Design = newDesign
+    .bind[Config].toInstance {
+      ConfigFactory
+        .parseString {
+          """
+          myapp.application.util.healthcheck {
+            interval = 1ms
+            healthy-threshold = 1
+            unhealthy-threshold = 1
+          }
+          """
+        }.withFallback(testKit.config)
+    }
+    .bind[JDBCHealthCheckApplication].toInstance(mock[JDBCHealthCheckApplication])
+
+  val mockApp: JDBCHealthCheckApplication     = diSession.build[JDBCHealthCheckApplication]
+  val service: JDBCHealthCheckService         = diSession.build[JDBCHealthCheckService]
+  val probe: TestProbe[GetCurrentStatusReply] = testKit.createTestProbe[GetCurrentStatusReply]()
+  val check: MockFunction0[Future[Boolean]]   = mockApp.check _
+
+  override def afterAll(): Unit = {
+    testKit.shutdownTestKit()
+    super.afterAll()
+  }
+
+  "JDBCHealthCheckService" should {
+    "return Healthy when it succeeded in db connection" in {
+      check.expects().returns(Future.successful(true)).anyNumberOfTimes()
+      val healthChecker: ActorRef[Command] = testKit.spawn(service.createBehavior())
+      Thread.sleep(100)
+      healthChecker ! GetCurrentStatus(probe.ref)
+      probe.expectMessage(Healthy)
+    }
+
+    "return Unhealthy when it failed to db connection" in {
+      check.expects().returns(Future.successful(false)).anyNumberOfTimes()
+      val healthChecker: ActorRef[Command] = testKit.spawn(service.createBehavior())
+      Thread.sleep(100)
+      healthChecker ! GetCurrentStatus(probe.ref)
+      probe.expectMessage(Unhealthy)
+    }
+  }
+}
