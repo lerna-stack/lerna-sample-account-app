3eeea667cc3469d7461e1f79cd8f18d5360a67a0
Naoki Yamada <yamada.naoki@tis.co.jp>
2021-12-10 17:51:19 +0900

    feat: ヘルスチェックの設定値を読み込む実装を追加

--- app/application/src/main/scala/myapp/application/util/healthcheck/JDBCHealthCheckServiceSettings.scala
+++ app/application/src/main/scala/myapp/application/util/healthcheck/JDBCHealthCheckServiceSettings.scala
@@ -3,13 +3,15 @@ package myapp.application.util.healthcheck
 import com.typesafe.config.Config
 
 import scala.concurrent.duration.FiniteDuration
+import scala.jdk.DurationConverters.JavaDurationOps
 
 class JDBCHealthCheckServiceSettings(root: Config) {
 
-  val interval: FiniteDuration = ???
+  private[this] val config     = root.getConfig("myapp.application.util.healthcheck")
+  val interval: FiniteDuration = config.getDuration("interval").toScala
 
-  val healthyThreshold: Int = ???
+  val healthyThreshold: Int = config.getInt("healthy-threshold")
 
-  val unhealthyThreshold: Int = ???
+  val unhealthyThreshold: Int = config.getInt("unhealthy-threshold")
 
 }
