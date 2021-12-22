2c3e4392c8c5035c0378b86d2c957fda644e58f4
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2021-12-22 10:57:55 +0900

    WIP Update akka-entity-replication to 2.1.0

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,9 +3,9 @@ import sbt._
 object Dependencies {
 
   object Versions {
-    val akkaEntityReplication    = "2.0.0"
+    val akkaEntityReplication    = "2.0.0+104-36743bd9-SNAPSHOT"
     val lerna                    = "3.0.0"
-    val akka                     = "2.6.12"
+    val akka                     = "2.6.17"
     val akkaHttp                 = "10.2.4"
     val akkaPersistenceCassandra = "1.0.1"
     val akkaProjection           = "1.1.0"
