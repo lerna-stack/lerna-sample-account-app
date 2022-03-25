0b05e8db882e9b15df4ab2fd9ea7bcfb77375ed7
Taichi Yamakawa <yamakawa.taichi@tis.co.jp>
2022-03-25 09:33:36 +0900

    lerna-app-library を v3.0.1 に更新する

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Versions {
     val akkaEntityReplication    = "2.1.0"
-    val lerna                    = "3.0.0-6-ca3f2b2b-SNAPSHOT"
+    val lerna                    = "3.0.1"
     val akka                     = "2.6.17"
     val akkaHttp                 = "10.2.4"
     val akkaPersistenceCassandra = "1.0.1"
